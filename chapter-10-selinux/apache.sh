631919e4dc82505c1bcf7c260cfd74cade1764ca
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2022-05-14 23:38:48 +0200

    Add apache script

---
+++ chapter-10-selinux/apache.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install httpd
+dnf -y install httpd
+
+# Enable firewall for services
+for service in http https; do
+	# Save to be the default
+	firewall-cmd --add-service=${service} --permanent
+
+	# Apply to current session
+	firewall-cmd --add-service=${service}
+done
+
+# Start and enable services on boot
+for service in httpd; do
+	systemctl start ${service}
+	systemctl enable ${service}
+done
